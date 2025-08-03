a477a0b6ac5fa1e8c5b54a209399fd8a8e990853
Mo'men <mmm.isam.99@gmail.com>
2025-08-03 15:05:06 +0200

    Switch to db push for deployment

--- src/app/utils/DOMIN.ts
+++ src/app/utils/DOMIN.ts
@@ -1,1 +1,2 @@
-export const domin_name = `https://wiki-app-git-main-momen-xs-projects.vercel.app/register`;
+export const domin_name = `https://wiki-app-nine.vercel.app/register`;
+// https://wiki-app-git-main-momen-xs-projects.vercel.app/register/api/users/register
