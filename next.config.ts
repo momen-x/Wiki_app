f560d0bdd19f786419b33c71c1ef969b0aac1643
Mo'men <mmm.isam.99@gmail.com>
2025-08-04 10:04:54 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+// typescript: {
+//     ignoreBuildErrors: true, // Not recommended for production
+//   },};
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-typescript: {
-    ignoreBuildErrors: true, // Not recommended for production
-  },};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  compress: true,
+  poweredByHeader: false,
+};
 
 export default nextConfig;
