0eeb7dc293b5bb741d84bf58cb45b45a1d4acc91
Mo'men <mmm.isam.99@gmail.com>
2025-08-04 10:20:06 +0200

    production

--- src/app/utils/DOMIN.ts
+++ src/app/utils/DOMIN.ts
@@ -1,2 +1,5 @@
-export const domin_name = `https://wiki-app-nine.vercel.app/register`;
+export const domin_name =
+  process.env.NODE_ENV === "production"
+    ? "https://wiki-app-nine.vercel.app"
+    : "http://localhost:3000";
 // https://wiki-app-git-main-momen-xs-projects.vercel.app/register/api/users/register
