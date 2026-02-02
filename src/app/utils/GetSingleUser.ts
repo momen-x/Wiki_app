04bb269fd07c8585ace9d2fd152a45806c026eae
Mo'men <mmm.isam.99@gmail.com>
2026-02-02 15:24:23 +0200

    deleting unused files and change the domain

--- src/app/utils/GetSingleUser.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { domain_name } from "./Domain";
-
-export const GetSingleUser = async (id: any) => {
-  const response = await axios.get(`${domain_name}/api/users/profile/${id}`);
-};
