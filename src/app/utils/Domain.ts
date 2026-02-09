abf69c4fd587da68534e06cdc810c1b614a159f5
Mo'men <mmm.isam.99@gmail.com>
2026-02-09 12:24:09 +0200

    edit the domain name

--- src/app/utils/Domain.ts
+++ src/app/utils/Domain.ts
@@ -1,4 +1,2 @@
-export const domain_name ="http://localhost:3000";
-//  process.env.NEXT_PUBLIC_API_URL ||"https://wiki-app-seven.vercel.app"
-
+export const domain_name =  process.env.NEXT_PUBLIC_API_URL;
 
