2170c5a87d77dd1486415eb9c80ca3fd8d327924
Mo'men <mmm.isam.99@gmail.com>
2026-02-09 12:07:33 +0200

    delete the middlewere

--- src/auth.ts
+++ src/auth.ts
@@ -34,3 +34,14 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 });
 
 export default auth;
+
+// import NextAuth from "next-auth";
+// import { authConfig } from "./auth.config";
+
+// export default NextAuth(authConfig).auth;
+
+// export const config = {
+//   matcher: [
+//     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)',
+//   ],
+// };
